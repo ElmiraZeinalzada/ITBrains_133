17b0f7b3407f47f06de1d1ba90c465de665b86bb
ElmiraZeinalzada <elmirazeynalzad@gmail.com>
2023-08-10 22:40:21 +0400

    Add to local

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Elmira");
+console.log("My name is \"Elmira\"");
+
+const name="Elmira";
+console.log(name);
+console.log("My name is" + name);
+// Template literals
+// STring template
+console.log(`My name is ${name}`);
+
+
+let text = "3";
+let num =Number(text);
+console.log(num)
