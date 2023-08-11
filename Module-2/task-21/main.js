e200fff576f1d10c2d67d9e8536f11980ce46a22
ElmiraZeinalzada <elmirazeynalzad@gmail.com>
2023-08-11 21:52:22 +0400

    feat:task added

---
+++ Module-2/task-21/main.js
@@ -0,0 +1,7 @@
+let data = prompt("Zehmet olmasa soyad,ad ve ata adinizi daxil edin");//zeynalzade elmira mehdi
+let space = data.slice(data.indexOf(" ") + 1);
+let surname = data.slice(0, data.indexOf(" "))
+let name = data.slice(data.indexOf(" "),data.lastIndexOf(" "))
+let secondName=data.slice(data.lastIndexOf(" "))
+
+console.log(`${surname} ${name[1]}.${secondName[1]}`);
